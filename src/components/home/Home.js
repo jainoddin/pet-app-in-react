b037d1c57389d8125322c91d18ca57787cc235c4
jainoddin <skjainoddin39854@gmail.com>
2024-05-25 19:46:03 +0530

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.css'
+import img1 from "../../img/dogg.jpg"
+
+const Home = () => {
+  return (
+    <div>
+        <section>
+           
+        <div className='text-img'>
+            <h1>Find your new pet</h1>
+            <p>Discover a variety of pets waiting for there forever homes. Stop by in <br></br>Telangana, TS today.</p>
+
+        </div>
+        </section>
+    </div>
+  )
+}
+
+export default Home

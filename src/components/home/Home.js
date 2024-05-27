2c2b9f3baf9240f51067521f87382f12dc41a044
jainoddin <skjainoddin39854@gmail.com>
2024-05-27 17:48:15 +0530

    Your commit message

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,20 +1,21 @@
-import React from 'react'
-import './Home.css'
-import img1 from "../../img/dogg.jpg"
+import React from "react";
+import "./Home.css";
+import img1 from "../../img/dogg.jpg";
 
 const Home = () => {
   return (
     <div>
-        <section>
-           
-        <div className='text-img'>
-            <h1>Find your new pet</h1>
-            <p>Discover a variety of pets waiting for there forever homes. Stop by in <br></br>Telangana, TS today.</p>
-
+      <section>
+        <div className="text-img">
+          <h1>Find your new pet</h1>
+          <p>
+            Discover a variety of pets waiting for there forever homes. Stop by
+            in <br></br>Telangana, TS today.
+          </p>
         </div>
-        </section>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
